7e755b015e021d91bc88f9aae673312fb9722d05
Sergei12677 <gorod_26@icloud.com>
2024-11-14 22:15:12 +0500

    Add files via upload

---
+++ module_5_4.py
@@ -0,0 +1,40 @@
+from doctest import Example
+
+
+class House:
+
+    def __new__ (cls,*args,**kwargs):
+        print(args)
+        print(kwargs)
+        return object.__new__(cls)
+
+    def __init__(self,first,second,third):
+        print(first)
+        print(second)
+        print(third)
+
+    houses_history = []
+
+    def __new__ (cls,*args):
+        cls.houses_history.append(args[0])
+        return object.__new__(cls)
+
+    def __init__(self,name,number_of_floors):
+        self.name=name
+        self.number_of_floors = number_of_floors
+
+
+    def __del__(self):
+        print(f'{self.name} снесен, но он останется в истории')
+
+h1 = House ('ЖК Эльбрус',10)
+print(House.houses_history)
+h2 = House ('ЖК Акация',20)
+print(House.houses_history)
+h3 = House ('ЖК Матрешки',20)
+print(House.houses_history)
+
+del h1
+del h2
+
+print(House.houses_history)
